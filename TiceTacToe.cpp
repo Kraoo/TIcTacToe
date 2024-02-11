2e1a8f5921fa60a77a4146e8a6218e1796e89282
iann <imkrao699@gmail.com>
2024-02-11 14:07:40 -0600

    reformat and changes

--- TiceTacToe.cpp
+++ TiceTacToe.cpp
@@ -3,174 +3,134 @@
 
 using namespace std;
 
-char space[3][3]={{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-char token = 'x';
+char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+char currentPlayer = 'X';
 
-int row;
-int column;
+string player1Name;
+string player2Name;
 
-string n1 = "";
-string n2 = "";
+bool isTie = false;
 
-bool tie = false;;
+void displayBoard();
+void getPlayerNames();
+void makeMove();
+bool checkWin();
+bool checkTie();
+bool playAgain();
 
+int main() {
+    getPlayerNames();
 
-void functionOne()
-{  
-    cout << " "<<space[0][0]<<"  | "<<space[0][1]<<"  | "<<space[0][2]<<"  \n";
-    cout << " ---|----|---\n";
-    cout << " "<<space[1][0]<<"  | "<<space[1][1]<<"  | "<<space[1][2]<<"  \n";
-    cout << " ---|----|---\n";
-    cout << " "<<space[2][0]<<"  | "<<space[2][1]<<"  | "<<space[2][2]<<"  \n";
+    bool replay = true;
+    while (replay) {
+        currentPlayer = 'X';
+        isTie = false;
 
+        while (!checkWin() && !isTie) {
+            displayBoard();
+            makeMove();
+        }
+
+        displayBoard();
+
+        if (isTie) {
+            cout << "It's a tie!\n";
+        } else {
+            if (currentPlayer == 'X') {
+                cout << player2Name << " wins!\n";
+            } else {
+                cout << player1Name << " wins!\n";
+            }
+        }
+
+        replay = playAgain();
+    }
+
+    return 0;
 }
 
-void functionTwo()
-{
-    int digit;
-
-    if (token=='x')
-    {
-        cout<<n1<<"Please Enter: ";
-        cin>>digit;
-    }
-
-    if (token=='0')
-    {
-        cout<<n2<<"Please Enter: ";
-        cin>>digit;
-    }
-
-    if (digit==1)
-    {
-        row=0;
-        column=0;
-    }
-
-    if (digit==2)
-    {
-        row=0;
-        column=1;
-    }
-
-    if (digit==3)
-    {
-        row=0;
-        column=2;
-    }
-
-    if (digit==4)
-    {
-        row=1;
-        column=0;
-    }
-
-    if (digit==5)
-    {
-        row=1;
-        column=1;
-    }
-
-    if (digit==6)
-    {
-        row=1;
-        column=2;
-    }
-
-    if (digit==7)
-    {
-        row=2;
-        column=0;
-    }
-
-    if (digit==8)
-    {
-        row=2;
-        column=1;
-    }
-
-    if (digit==9)
-    {
-        row=2;
-        column=2;
-    }
-    
-    else if(digit<1 || digit>9)
-    {
-        cout<<"Invalid!!"<<endl;
-    }
-
-    if (token=='x' && space[row][column] !='x' && space[row][column] !='0')
-    {
-        space[row][column]='x';
-        token='0';
-    }
-
-    else if(token=='0' && space[row][column] !='x' && space[row][column] !='0')
-    {
-        space[row][column]='0';
-        token='x';
-    }
-
-    else
-    {
-        cout<<"There is no space"<<endl;
-        functionTwo();
-    }
-
-    functionOne();
-
+void displayBoard() {
+    system("cls");
+    cout << "  " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " \n";
+    cout << " ---|---|---\n";
+    cout << "  " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " \n";
+    cout << " ---|---|---\n";
+    cout << "  " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " \n";
 }
 
-bool functionThree()
-{
-    for (int i=0; i<3; i++)
-    {
-        if (space[i][0]==space[i][1] && space[i][0]==space[i][2] || space[0][i]==space[1][i] && space[0][i]==space[2][i])
+void getPlayerNames() {
+    cout << "Enter the name of the first player: ";
+    getline(cin, player1Name);
+    cout << "Enter the name of the second player: ";
+    getline(cin, player2Name);
+}
+
+void makeMove() {
+    int position;
+    cout << (currentPlayer == 'X' ? player1Name : player2Name) << ", enter your move (1-9): ";
+    cin >> position;
+
+    if (position < 1 || position > 9) {
+        cout << "Invalid move! Please enter a number between 1 and 9.\n";
+        return;
+    }
+
+    int row = (position - 1) / 3;
+    int col = (position - 1) % 3;
+
+    if (board[row][col] == 'X' || board[row][col] == 'O') {
+        cout << "That position is already taken! Please choose another.\n";
+        return;
+    }
+
+    board[row][col] = currentPlayer;
+
+    if (checkWin()) {
+        return;
+    }
+
+    if (checkTie()) {
+        isTie = true;
+        return;
+    }
+
+    currentPlayer = (currentPlayer == 'X' ? 'O' : 'X');
+}
+
+bool checkWin() {
+    for (int i = 0; i < 3; ++i) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+            return true;
+        }
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+            return true;
+        }
+    }
+
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+        return true;
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
         return true;
     }
 
-        if(space[0][0]==space[1][1] && space[1][1]==space[2][2] || space[0][2]==space[1][1] && space[1][1]==space[2][0])
-    {
-        return true;
-    }
+    return false;
+}
 
-    for (int i=0; i<3; i++)
-    {
-        for  (int j=0; j<3; j++)
-        {
-            if (space[i][j] != 'x' && space[i][j] != '0')
-            {
+bool checkTie() {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i][j] != 'X' && board[i][j] != 'O') {
                 return false;
             }
         }
     }
-
+    return true;
 }
 
-
-int main()
-{
-    cout << "Enter the name of the first player: \n";
-        getline(cin,n1);
-    cout << "Enter the name of the second player: \n";
-        getline(cin,n2);
-    cout << n1 << "is player one so they will go first \n";
-    cout << n2 << "is player two so they will go second \n";
-
-
-        while (!functionThree())
-    {
-        functionOne();
-        functionTwo();
-        functionThree();
-
-    }
-
-    if (token=='x' && tie == false)
-    {
-        cout<<n2<<"Wins!"<<endl;
-    }
-    
-    return 0;
+bool playAgain() {
+    char choice;
+    cout << "Do you want to play again? (y/n): ";
+    cin >> choice;
+    return (choice == 'y' || choice == 'Y');
 }
